f5564e1b8f2add04d3338e8ddbed11083cf79281
Marvin Strangfeld <marvin@strangfeld.io>
2025-05-31 00:08:02 +0200

    docs: Add mkdocs social feature

--- devenv.nix
+++ devenv.nix
@@ -8,10 +8,13 @@
     pkgs.ruff
     pkgs.zlib
     pkgs.python312Packages.pandas
+    pkgs.cairo
   ];
 
   languages.python = {
     enable = true;
     uv.enable = true;
   };
+
+  dotenv.enable = true;
 }
